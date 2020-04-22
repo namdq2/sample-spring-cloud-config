7c08fca2d7576c1138625863612a4ca7330f1cbe
NamDQ <namdq@smartosc.com>
2020-04-22 11:21:13 +0700

    init project

---
+++ configuration-client/src/main/java/com/namdq/configurationclient/ConfigurationClientApplication.java
@@ -0,0 +1,13 @@
+package com.namdq.configurationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationClientApplication.class, args);
+	}
+
+}
