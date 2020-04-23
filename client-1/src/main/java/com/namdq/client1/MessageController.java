0ef02496377612b20db37d5f6ea2ea158eee5bc0
NamDQ <namdq@smartosc.com>
2020-04-23 14:07:06 +0700

    add client 1

---
+++ client-1/src/main/java/com/namdq/client1/MessageController.java
@@ -0,0 +1,25 @@
+package com.namdq.client1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @Value("${message:default}")
+    private String message;
+
+    @Value("${name:default}")
+    private String name;
+
+    @Value("${environment:default}")
+    private String environment;
+
+    @Value("${global:default}")
+    private String global;
+
+    @GetMapping("/message")
+    public Message getMessage() {
+        return new Message(message, name, environment, global);
+    }
+}
