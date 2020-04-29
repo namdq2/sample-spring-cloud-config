4be55426c192af53ea1d75476e17a7b0bcdd1aff
NamDQ <namdq@smartosc.com>
2020-04-29 17:02:20 +0700

    test update config while process use static var

--- client-2/src/main/java/com/namdq/client2/MessageController.java
+++ client-2/src/main/java/com/namdq/client2/MessageController.java
@@ -1,13 +1,17 @@
 package com.namdq.client2;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class MessageController {
-    @Value("${message:default}")
-    private String message;
+//    @Value("${message:default}")
+//    private String message;
+
+    private static String message;//MESSAGE_STATIC;
 
     @Value("${name:default}")
     private String name;
@@ -20,6 +24,22 @@ public class MessageController {
 
     @GetMapping("/message")
     public Message getMessage() {
+        String msg = message;
+        System.out.println(msg);
+        System.out.println("Sleep");
+        try {
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        msg = message;
+        System.out.println(msg);
         return new Message(message, name, environment, global);
     }
+
+    @Value("${message}")
+    public void setMessageStatic(String message){
+        MessageController.message = message;
+    }
+
 }
